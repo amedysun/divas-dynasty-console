8d17a44aed1d13acc3f4aa884000e7b96fa3972f
Pecataaa <83838371+amedysun@users.noreply.github.com>
2024-08-22 21:36:22 +0300

    feature: Add Options as console input and register logger.

---
+++ src/divas-dynasty/Options.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+
+namespace divas_dynasty;
+
+internal class Options
+{
+    [Option('d', "deposit", Required = false, HelpText = "Deposit amount.")]
+    public decimal? DepositAmount { get; set; }
+
+    [Option('w', "withdraw", Required = false, HelpText = "Withdraw amount.")]
+    public decimal? WithdrawAmount { get; set; }
+
+    [Option('b', "bet", Required = false, HelpText = "Bet amount.")]
+    public decimal? BetAmount { get; set; }
+
+    [Option('e', "exit", Required = false, HelpText = "Exit the game.")]
+    public bool Exit { get; set; }
+}
--- src/divas-dynasty/Program.cs
+++ src/divas-dynasty/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Serilog;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+           .WriteTo.Console()
+           .CreateLogger();
+    }
+}
