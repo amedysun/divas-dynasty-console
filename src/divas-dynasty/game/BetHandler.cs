98d17a2b8e83d27cf4989c1b8700ba48dbaddf35
Pecataaa <83838371+amedysun@users.noreply.github.com>
2024-09-17 22:09:35 +0300

    fix: Use wallet balance directly instead of assign it to variable.

--- src/divas-dynasty/game/BetHandler.cs
+++ src/divas-dynasty/game/BetHandler.cs
@@ -9,13 +9,12 @@ public class BetHandler(Wallet wallet, Game game, IValidator<BettingData> valida
     public void Handle(BetCommand command)
     {
         var betAmount = command.Amount;
-        var walletBalance = wallet.Balance;
-        validator.Validate(new BettingData(betAmount, walletBalance));
+        validator.Validate(new BettingData(betAmount, wallet.Balance));
         BetResult betResult = game.PlaceBet(betAmount);
         wallet.UpdateBalance(betAmount, betResult.WinAmount);
 
         Log.Information(betResult.IsWin
-            ? $"You won: ${betResult.WinAmount:F2}. Current balance: ${walletBalance:F2}"
-            : $"You lost the bet. Current balance: ${walletBalance:F2}");
+            ? $"You won: ${betResult.WinAmount:F2}. Current balance: ${wallet.Balance:F2}"
+            : $"You lost the bet. Current balance: ${wallet.Balance:F2}");
     }
 }
